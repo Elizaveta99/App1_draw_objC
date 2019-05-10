ad6e46a1f2f3eee31d15cdb8c48e5a6b17b9b856
eliz <eliz.vitebsk@gmail.com>
2019-05-10 13:47:48 -0700

    .

--- application_1/ViewController.m
+++ application_1/ViewController.m
@@ -11,7 +11,12 @@
 @interface ViewController ()
 
 @property (strong, nonatomic) IBOutlet UIImageView *canvas;
+@property (strong, nonatomic) IBOutlet UIImageView *colorsImage;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *colorsSegmentedControl;
+@property (strong, nonatomic) IBOutlet UISwitch *formSwitch;
+@property (strong, nonatomic) IBOutlet UISlider *sizeSlider;
 
+@property NSArray* colorsArray;
 @property CGPoint lastPoint;
 
 @end
@@ -20,7 +25,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.colorsArray = [NSArray arrayWithObjects: UIColor.purpleColor, UIColor.blueColor, UIColor.greenColor, UIColor.orangeColor, UIColor.redColor, nil];
+    
+    UIImage* colors = [UIImage imageNamed: @"colors.png"]; //??
+    self.colorsImage = [[UIImageView alloc] initWithFrame:self.colorsImage.frame];
+    self.colorsImage.contentMode = UIViewContentModeScaleAspectFit;
+    self.colorsImage.image = colors;
+    [self.view addSubview:self.colorsImage];
+
+}
+- (IBAction)formSwitchTapped:(id)sender {
+    if (self.formSwitch.isOn)
+    {
+        CGContextSetLineDash(UIGraphicsGetCurrentContext(), 0, (CGFloat[]){1}, 0);
+    }
+    else {
+        CGContextSetLineDash(UIGraphicsGetCurrentContext(), 0, (CGFloat[]){2, 1}, 2);
+    }
+}
+
+- (IBAction)saveTapped:(id)sender {
+    NSData* data = UIImagePNGRepresentation(_canvas.image);
+    [data writeToFile:@"/Users/eliz/Desktop/TP/lab8/application_1/drawed_images/picture.png" atomically:YES];
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -36,8 +62,21 @@
                                  self.view.frame.size.height);
     [[[self canvas] image] drawInRect:drawRect];
     CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
-    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 5.0f);
-    CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), 1.0f, 0.0f, 0.0f, 1.0f);
+    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), (int)_sizeSlider.value);
+    
+    UIColor* c = [[self colorsArray] objectAtIndex:(int)self.colorsSegmentedControl.selectedSegmentIndex];
+    const CGFloat* comp =  CGColorGetComponents(c.CGColor);
+    CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), comp[0], comp[1], comp[2], CGColorGetAlpha(c.CGColor));
+    
+    
+    if (self.formSwitch.isOn)
+    {
+        CGContextSetLineDash(UIGraphicsGetCurrentContext(), 0, (CGFloat[]){1}, 0);
+    }
+    else {
+        CGContextSetLineDash(UIGraphicsGetCurrentContext(), 0, (CGFloat[]){2, 1}, 2);
+    }
+    
     CGContextBeginPath(UIGraphicsGetCurrentContext());
     CGContextMoveToPoint(UIGraphicsGetCurrentContext(), _lastPoint.x,
                          _lastPoint.y);
